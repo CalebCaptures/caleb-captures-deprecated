94e65c2fbf7d978449c5282819d99eaa736afa7e
Richard Lai <richardlai@Richards-MacBook-Pro.local>
2025-11-27 02:41:50 +1000

    Add bunch of changes

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -8,18 +8,186 @@ const idStreetTab = "streetTab";
 /* Class Constants */
 const classHide = "hide";
 const classAll = "all";
-const classPortraiture = "portraiture";
+const classPortraiture = "portrait";
 const classEvent = "event";
 const classVenue = "venue";
 const classStreet = "street";
 
 /* List of Elements */
-const listAllImages = document.querySelectorAll("#portfolio img");
+
+/* List of Photos */
+/* Events */
+const qpsa_pharmacy_ball_2025_09_06 = [
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC00904.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC00957.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC00961.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC00962.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC01024.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC01057.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC01065.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC01079.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC01106.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC01113.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC01124.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC01125.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC01147.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC01162.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC01179.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_09_06_qpsa_pharmacy_ball/DSC01207.jpg", category: "events" }
+];
+
+const uqba_uqmas_marketing_panel_2025_10_08 = [
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC08647.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC08650.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC08686.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC08690.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC08697.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC08785.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC08880.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC08899.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC08920.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC08928.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC09039.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC09101.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC09174.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC09181.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_08_uqba_uqmas_marketing_panel/DSC09247.jpg", category: "events" }
+];
+
+const uqdance_ball_2025_10_11 = [
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00026.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00057.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00074.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00125.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00137.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00236.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00334.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00350.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00373.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00412.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00493.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00589.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00642.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00661.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00794.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00813.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC00907.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_11_uqdance_ball/DSC09938.jpg", category: "events" }
+];
+
+const uqsla_uqnc_fright_night_list_2025_10_31 = [
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC05931.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC05944.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC05971.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06007.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06078.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06099.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06132.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06215.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06319.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06431.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06480.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06578.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06655.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06729.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06739.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06847.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06872.jpg", category: "events" },
+    { src: "../images/portfolio/events/2025_10_31_uqsla_uqnc_fright_night/DSC06946.jpg", category: "events" }
+];
+
+/* List of Portrait */
+const portrait_2025_11_27 = [
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/1.jpg", category: "portrait" },
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/2.jpg", category: "portrait" },
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/3.jpg", category: "portrait" },
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/4.jpg", category: "portrait" },
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/5.jpg", category: "portrait" },
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/6.jpg", category: "portrait" },
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/7.jpg", category: "portrait" },
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/8.jpg", category: "portrait" },
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/9.jpg", category: "portrait" },
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/10.jpg", category: "portrait" },
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/11.jpg", category: "portrait" },
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/12.jpg", category: "portrait" },
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/13.jpg", category: "portrait" },
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/14.jpg", category: "portrait" },
+    { src: "../images/portfolio/portrait/2025_11_27_portrait/15.jpg", category: "portrait" }
+];
+const listEventsImages = [
+    ...qpsa_pharmacy_ball_2025_09_06,
+    ...uqba_uqmas_marketing_panel_2025_10_08,
+    ...uqdance_ball_2025_10_11,
+    ...uqsla_uqnc_fright_night_list_2025_10_31
+]
+
+const listPortraitImages = [
+    ...portrait_2025_11_27,
+]
+
+let listAllImages = [
+    ...listEventsImages,
+    ...listPortraitImages,
+]
+
+shuffle(listAllImages);
 
 /* Variables */
 let hasAddedEventListeners = false;
 
 /* Functions */
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function populateGalleryColumns() {
+    const firstGalleryColumn = document.createDocumentFragment();
+    const secondGalleryColumn = document.createDocumentFragment();
+    const thirdGalleryColumn = document.createDocumentFragment();
+
+    const totalImages = listEventsImages.length + listPortraitImages.length;
+    const firstDivisionIndex = Math.floor(totalImages / 3 - 1);
+    const secondDivisionIndex = Math.floor(firstDivisionIndex * 2 - 1);
+    const thirdDivisionIndex = Math.floor(firstDivisionIndex * 3 - 1);
+
+    for (let imageIndex = 0; imageIndex < firstDivisionIndex; imageIndex++) {
+        const image = listAllImages[imageIndex];
+        const img = document.createElement('img');
+        img.src = image.src;
+        img.classList.add(image.category);
+        firstGalleryColumn.appendChild(img);
+    }
+
+    for (let imageIndex = firstDivisionIndex; imageIndex < secondDivisionIndex; imageIndex++) {
+        const image = listAllImages[imageIndex];
+        const img = document.createElement('img');
+        img.src = image.src;
+        img.classList.add(image.category);
+        secondGalleryColumn.appendChild(img);
+    }
+
+    for (let imageIndex = secondDivisionIndex; imageIndex < thirdDivisionIndex; imageIndex++) {
+        console.log(imageIndex);
+        const image = listAllImages[imageIndex];
+        const img = document.createElement('img');
+        img.src = image.src;
+        img.classList.add(image.category);
+        thirdGalleryColumn.appendChild(img);
+    }
+
+    const firstColumnDiv = document.getElementById('gallery-column-1');
+    const secondColumnDiv = document.getElementById('gallery-column-2');
+    const thirdColumnDiv = document.getElementById('gallery-column-3');
+
+    firstColumnDiv.appendChild(firstGalleryColumn);
+    secondColumnDiv.appendChild(secondGalleryColumn);
+    thirdColumnDiv.appendChild(thirdGalleryColumn);
+}
+
 function addClassDisplay(className) {
     listAllImages.forEach(element => {
         const hasClassName = element.classList.contains(className);
@@ -78,4 +246,5 @@ function addAllTabEventListeners() {
     hasAddedEventListeners = true;
 }
 
+populateGalleryColumns();
 addAllTabEventListeners();
